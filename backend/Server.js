16dc66c861a7669c479ae95550572a6ddc86d02a
swathisundar22 <swathisundar223@gmail.com>
2026-08-14 17:37:42 +0530

    second changes

--- backend/Server.js
+++ backend/Server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 
 const connectDB = require("./config/db");
-const studentRoutes = require("./routes/studentRoutes");
+const studentRoutes = require("./routes/StudentRoutes");
 
 const app = express();
 
